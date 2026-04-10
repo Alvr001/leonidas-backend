7156196df07a347e08393430ba978c349a86cd99
Alvr001 <alvaro.caldas12@gmail.com>
2026-04-10 13:09:44 -0500

    Config env vars for Render

--- src/main/java/com/alpheratz/config/FirebaseConfig.java
+++ src/main/java/com/alpheratz/config/FirebaseConfig.java
@@ -1,11 +1,15 @@
 package com.alpheratz.config;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.springframework.context.annotation.Configuration;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Configuration;
+
 import jakarta.annotation.PostConstruct;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -14,9 +18,7 @@ public class FirebaseConfig {
     public void init() {
         try {
             if (FirebaseApp.getApps().isEmpty()) {
-                InputStream serviceAccount =
-                    getClass().getClassLoader()
-                        .getResourceAsStream("firebase-service-account.json");
+                InputStream serviceAccount = new FileInputStream("/etc/secrets/firebase-service-account.json");
 
                 FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
